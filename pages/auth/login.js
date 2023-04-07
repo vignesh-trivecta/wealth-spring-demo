cb0c6b588c5c5279482425249c9129b08de0d22f
Rajesh <rajesh.ra@trivectadigital.com>
2023-04-07 18:07:35 +0530

    implemented jwt for safe authentication

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
+import jwt  from "jsonwebtoken";
 import { useContext, useState, } from "react";
 import { useRouter } from "next/router";
 import { userContext } from "../../contexts/useUserContext";
@@ -12,64 +13,41 @@ function LoginAuth() {
 
     const [loggedin, setLoggedIn] = useContext(userContext);
 
-
-    // onsubmit function - default version
-    // const submitLogin = async (e) => {
-    //     e.preventDefault();
-        // const res = await fetch('', {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json',
-        //     },
-        //     body: JSON.stringify({ user, password }),
-        // });
-
-        // const data = await res.json();
-
-
-        // handle login response from server
-        // if(true){
-        //     console.log("User Authorization success!");
-        //     setIsLoggedIn(!isLoggedIn);
-        //     sessionStorage.setItem('isLoggedIn', true);
-        //     router.push('../admin/dashboard');
-        // }
-        // else {
-        //     console.log("User doesn't exist");
-        //     alert("User doesn't exist");    
-        //     setUser('');
-        //     setPassword('');
-        // }
-    // }
-
     // onsubmit function - using useContext hook
     const submitLogin = (e) => {
         e.preventDefault();
-        console.log({user, password});
-        fetch("/adminlogin", {
+        
+        const token = jwt.sign({user, password}, process.env.SECRET_KEY);
+        console.log(token);
+
+        fetch("http://localhost:8082/adminlogin", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({ username, password }),
+            body: JSON.stringify({ token }),
         })
         .then(response => {
             if (response.status === 200) {
             // authentication successful
             // redirect user to dashboard or home page
-            console.log(response.status);
-            console.log(response.text());
+
+            // console.log(response.status);
+            // console.log(response.text());
             setLoggedIn(!loggedin);
             router.push('../admin/dashboard');
             } 
             else {
             // authentication failed
             // display error message to user
+
+            // console.log(response.status);
+            // console.log(response.text());
             alert("Invalid credentials");
             }
         })
         .then(data => {
-            console.log(data); // "true"
+            console.log(data); 
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
